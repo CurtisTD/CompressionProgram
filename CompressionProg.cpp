3f74f3fc512d48e34638108d635e2ed3e3c1881f
Curtis <curtissmp@gmail.com>
2020-02-07 00:50:39 -0500

    Uploaded Main Files
    Main file 1.) ImageCompress: Compresses images with Lempel-Ziv
    Main file 2.) CompressionProg and StringCompress should be linked, to add ImageCompress file later.

---
+++ CompressionProg.cpp
@@ -0,0 +1,30 @@
+/* 
+CompressionProg: 
+Main file for all compression/encoding files and algorithms.
+----------------------------------------
+Author: Curtis Davis
+*/
+
+#include "StringCompress.cpp"
+//#include "ImageCompress.cpp"
+
+int main(){
+
+    std::string stringTest = "";
+     std::cout << "Huffman Test:" << std::endl;
+    while(stringTest.empty()){
+        std::cout << "Enter a string of text(whitespace included): ";
+        getline(std::cin, stringTest);
+    }
+    //Sends user entered line into huffman compression
+    buildHuffTree(stringTest);
+
+/*LempelZiv is in ImageCompress.cpp*/
+/*Need to merge functions*/
+//    std::cout << std::endl << std::endl << "Lempel Ziv Text Test:" << std::endl;
+//    std::string lzCompressed = lzCompress("Hello, World!");
+//    std::cout << lzCompressed << std::endl;
+//    std::cout << lzDecompress(lzCompressed);
+
+    return EXIT_SUCCESS;
+}
